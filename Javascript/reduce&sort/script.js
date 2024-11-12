d01a2ccfd6d89bce34750e1d74b3901b13f24b17
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-12 11:52:31 +0100

    reduce & sort

---
+++ Javascript/reduce&sort/script.js
@@ -0,0 +1,12 @@
+let expenses= [100,200,300,400];
+
+let somma= expenses.reduce((accumulatore , currentValue) =>
+accumulatore + currentValue)
+
+console.log(somma)
+
+let words = ['banana','apple','cherry','date'];
+
+words.sort();
+
+console.log(words)
