20172c1f27f79fceac7f18ac516b6849cf4fb7e2
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-12 11:17:19 +0100

    forEach & map

---
+++ Javascript/forEach&map/script.js
@@ -0,0 +1,11 @@
+let numbers = [1,2,3,4,5]
+
+console.log(numbers)
+
+
+numbers.forEach((risultato) => 
+    console.log(risultato * 2))
+
+let squaredNumbers = numbers.map((risultato)=>risultato * 2)
+
+console.log(squaredNumbers)
