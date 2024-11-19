55009cbae534ad3aeab88dc70306079bdb4a28a7
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-19 22:19:14 +0100

    destructuring con nomi di variabili diverse

---
+++ Javascript/destructuringConNomiDiVariabiliDiverse/script.js
@@ -0,0 +1,11 @@
+let persona = {
+    nome:'pippo',
+    cognome: 'franco',
+    età: 50
+}
+
+let nome1 = persona.nome;
+let cognome1 = persona.cognome;
+let età1 = persona.età;
+
+console.log(nome1,cognome1,età1)
