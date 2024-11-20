82c335205e3c427097b9cbb3b1187a6d8ccb18f6
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-20 19:24:59 +0100

    manipolazione dei dati json

---
+++ Javascript/manipolazioneDiDatiJSON/script.js
@@ -0,0 +1,14 @@
+let list = ['{"mario":"rossi","giovanni":"rossini","nicola":"azzurro","luigi":"viola"}'];
+
+let newList = JSON.parse(list)
+
+let NewList = [{"marco":"biaco"}];
+
+list.push(NewList)
+
+listJson = JSON.stringify(list)
+
+console.log(listJson)
+
+
+
