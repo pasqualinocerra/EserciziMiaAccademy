d76ab2dbe28baeab829ac598a8dfc708c725cfb3
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-20 16:13:17 +0100

    copia di un oggetto con proprietà aggiuntive

---
+++ Javascript/copiaDiUnOggettoConPropietàAggiuntive/script.js
@@ -0,0 +1,10 @@
+const listaSpesa = {
+    carote : 4,
+    mele : 2,
+    mandarini : 3
+}
+
+const nuovaListaSpesa = {...listaSpesa,cipolla : 1}
+
+console.log(listaSpesa)
+console.log(nuovaListaSpesa)
