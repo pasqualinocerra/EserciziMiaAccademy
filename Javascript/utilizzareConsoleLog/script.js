7479a409f7e11608d6872fb86f50b62241b12064
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-21 10:03:19 +0100

    utilizzare console log

---
+++ Javascript/utilizzareConsoleLog/script.js
@@ -0,0 +1,13 @@
+let string = 'ciao mondo';
+let numbers = 30;
+let arr = [1,2,3,4,5]
+let obj = {
+    nome : 'mario',
+    cognome : 'rossi',
+    età : 90
+}
+
+console.log(string)
+console.log(numbers)
+console.log(arr)
+console.log(obj)
