95b931292546fbb373fd6aaacd00384153f0ac52
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-21 14:17:56 +0100

    utilizzare console table e console group

---
+++ Javascript/utilizzareConsoleTableEConsoleGroup/script.js
@@ -0,0 +1,10 @@
+let list = [{
+    auto : 'blu',
+    moto : 'gialla',
+    bici : 'nera',
+    monopattino : 'bianco'
+}];
+
+console.table(list)
+
+console.group('questa è una lista di oggetti',list,'fine della lista di oggetti')
