79f421bc1a4ebeefd251e969ca512f307516fedf
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-11-22 10:51:33 +0100

    usi di finally per eseguire codice indipendentemente dal risultato

---
+++ Javascript/usoDiFinallyPerEseguireCodiceIndipendentementeDalRisultato/script.js
@@ -0,0 +1,23 @@
+function testo(input) {
+    try {
+        if (typeof input !== 'string') {
+            throw new TypeError('L input deve essere una stringa!');
+        }
+
+        let risultato = input.toUpperCase();
+        console.log('Risultato:', risultato);
+        return risultato;
+
+    } catch (errore) {
+        if (errore instanceof TypeError) {
+            console.error('Errore di tipo:', errore.message);
+        } else {
+            console.error('Errore generico:', errore.message);
+        }
+    } finally {
+        
+        console.log('Esecuzione terminata.');
+    }
+}
+
+let risultato = testo(5)
