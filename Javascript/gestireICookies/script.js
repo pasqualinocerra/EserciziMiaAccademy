ff745b0538aa9cc4f17ea90e6aeee1be420a8609
pasqualinocerra <pasqualinocerra13@gmail.com>
2024-12-04 14:59:17 +0100

    gestire i cookies

---
+++ Javascript/gestireICookies/script.js
@@ -0,0 +1,5 @@
+document.cookie = ('giovanni=rossi');
+
+document.cookie = "giovanni=rossi; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+
+document.cookie = "giovanni=rossi; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
