8243a9b2a500cc4aee6b1a26f85486a8df75ba2c
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-08 22:28:05 +0100

    reset contatore

--- React/src/Counter.jsx
+++ React/src/Counter.jsx
@@ -16,13 +16,20 @@ function Counter() {
         })
     }
 
+    const reset = () => {
+        setCounter((_counter) => {
+        return _counter = 0;
+        })
+    }
+
     return (
       <>
        <div>
         Count : {counter}
        </div>
        <button onClick={increment}>INCREMENT</button><br/>
-       <button onClick={decrement}>DECREMENT</button>
+       <button onClick={decrement}>DECREMENT</button><br/>
+       <button onClick={reset}>RESET</button>
       </>
     )
   }
