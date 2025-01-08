92f673d70d03140cd87c0d1234fc768087ed6099
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-08 22:42:59 +0100

    sincronizza il titolo con il contatore

--- React/src/Counter.jsx
+++ React/src/Counter.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 function Counter() {
 
@@ -21,6 +21,10 @@ function Counter() {
         return _counter = 0;
         })
     }
+
+    useEffect(() => {
+         document.title = counter;
+    }, [counter])
 
     return (
       <>
