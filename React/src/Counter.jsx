b0381d886f52866530cc87c3a86266d795f0ec59
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-08 22:19:20 +0100

    incrementa contatore

--- React/src/Counter.jsx
+++ React/src/Counter.jsx
@@ -4,11 +4,18 @@ function Counter() {
 
     const [counter, setCounter] = useState(0);
 
+    const increment = () => {
+       setCounter((_counter) => {
+        return _counter + 1;
+       })
+    }
+
     return (
       <>
        <div>
-        Count : 0
+        Count : {counter}
        </div>
+       <button onClick={increment}>PUSH</button>
       </>
     )
   }
