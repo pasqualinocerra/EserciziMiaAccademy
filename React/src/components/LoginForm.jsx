8a1fcc558bb6aac54e765104e3cc6febe7d08881
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-10 15:06:16 +0100

    submit

--- React/src/components/LoginForm.jsx
+++ React/src/components/LoginForm.jsx
@@ -2,11 +2,39 @@ import {useState} from 'react'
 
 const LoginForm = () => {
 
-    const [username, setUsername] = useState();
-    const [password, setPassword] = useState();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const submit = (event) => {
+        event.preventDefault();
+        alert(`Username: ${username}`);
+        alert(`Password: ${password}`);
+    }
+
 
     return (
         <>
+        <form onSubmit={submit}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <br />
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
