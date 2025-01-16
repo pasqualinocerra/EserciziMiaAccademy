23a56ebc598d5c04796ed16e2903790c524d8ce3
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-16 11:37:15 +0100

    crea un hook per filtrare i to-do

---
+++ React2/src/assets/useFilteredTodos.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useFilteredTodos = (todos, search) => {
+
+    const [filtered, setFiltered] = useState([]);
+
+  useEffect(() => {
+    if (!todos || todos.length === 0) {
+      setFiltered([]);
+      return;
+    }
+
+    const lowerCaseSearch = search.toLowerCase(); 
+    const filteredTodos = todos.filter((todo) =>
+      todo.title.toLowerCase().includes(lowerCaseSearch)
+    ); 
+    setFiltered(filteredTodos); 
+  }, [todos, search]); 
+
+  return filtered;
+
+}
