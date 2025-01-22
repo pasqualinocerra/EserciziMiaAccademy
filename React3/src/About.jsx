337eb64167c84f70f02a9c94c78d8088936f11d7
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-01-23 00:02:29 +0100

    installato react.router.dom

---
+++ React3/src/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+
+    return (
+        <>
+        <h1>Organizzare la giornata</h1>
+        <p>Ti aiuta a partire con chiarezza, evitando di sentirti sopraffatto. Decidere le priorità rende tutto più gestibile.</p>
+        <h1>Lavoro/Studio</h1>
+        <p>Questa sezione è pensata per le tue attività professionali o accademiche. Include cose pratiche come completare un compito importante o rispondere alle comunicazioni.</p>
+        <h1>Casa</h1>
+        <p>Riguarda il mantenimento dell ambiente in cui vivi, perché una casa organizzata favorisce anche la concentrazione.</p>
+        </>
+    )
+
+}
+
+export default About
---
+++ React3/src/Home.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+
+    return (
+        <>
+        <ul> Lista To Do :
+            <li>Scrivere gli obiettivi principali.</li>
+            <li>Assegnare priorità.</li>
+            <li>Rispondere alle email/messaggi importanti.</li>
+            <li>Fare la spesa o una lista per gli acquisti.</li>
+        </ul>
+        </>
+    )
+    
+}
+
+export default Home
