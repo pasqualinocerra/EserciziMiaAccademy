fa7d5c28d5ac102b0a41b9261ae5bfb963aeb879
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-02-10 01:09:15 +0100

    aggiungi una funzione per aggiungere un nuovo todo

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -1,3 +1,14 @@
 import { Todo } from "./types"; 
 
 const todos: Todo[] = []; 
+
+const addTodo = (title: string): Todo => {
+    const newTodo: Todo = {
+      id: Date.now(), 
+      title,
+      completed: false,
+    };
+  
+    todos.push(newTodo); 
+    return newTodo;
+  };
