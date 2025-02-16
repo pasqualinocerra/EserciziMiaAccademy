450bb3a17f1314f10d72e590bb031f8cd46111d3
pasqualinocerra <pasqualinocerra13@gmail.com>
2025-02-16 18:15:24 +0100

    utilizzare record

--- TypeScript/src/utils.ts
+++ TypeScript/src/utils.ts
@@ -1,5 +1,16 @@
 import { Todo } from './types'; 
 
+export type TodoRecord = {
+    [id: number]: Todo;
+  };
+  
+  export const convertArrayToRecord = (todos: Todo[]): TodoRecord => {
+    return todos.reduce((acc, todo) => {
+      acc[todo.id] = todo;
+      return acc;
+    }, {} as TodoRecord);
+  };
+
 function filterTodos(todos: Todo[], filterFn: (todo: Todo) => boolean): Todo[] {
   return todos.filter(filterFn);
 }
